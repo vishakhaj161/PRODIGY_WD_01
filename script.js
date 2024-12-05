671dc17dc3f0649f9207cd6eb29b4473c70ca972
vishakhaj161 <jv151655@gmail.com>
2024-12-05 10:43:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar style on scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
